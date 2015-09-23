501e335f3477234e87c92bc2246cbbd8f2b8cba2
thejereman13 <jereman13@gmail.com>
2015-09-23 18:13:17 -0400

    Isometric World Work
    Input Fields are not working for me....

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+		if (horizontalMovement != 0) {
+			transform.Translate (horizontalMovement, 0, 0);
+		}
+		if (verticalMovement != 0) {
+			transform.Translate (0, 0, verticalMovement);
+		}
+	}
+}
---
+++ Assets/Scripts/Menus/WorldMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WorldMovement : MonoBehaviour {
+	public GameObject cameraBox;
+	public int xCord;
+	public int yCord;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void positionMovement(){
+		Transform cameraTransform = cameraBox.GetComponent<Transform> ();
+		cameraTransform.position = new Vector3(xCord, 1, yCord);
+	}
+	public void xChange(){
+		GameObject xBox = GameObject.Find ("X-Cord");
+		InputField input = xBox.GetComponent<InputField> ();
+		if (input.text != null){
+			xCord = int.Parse (input.text);
+		}
+	}
+	public void yChange(){
+		GameObject xBox = GameObject.Find ("Y-Cord");
+		InputField input = xBox.GetComponent<InputField> ();
+		yCord = int.Parse (input.text);
+	}
+}
