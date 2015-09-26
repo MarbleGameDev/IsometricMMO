61dcbb15cf8919a7c59c2768e643e58864327b03
thejereman13 <jereman13@gmail.com>
2015-09-26 19:15:32 -0400

    Menu Popup Sidebar is now functional
    I got the popup window to toggle with button presses as well as switch
    panels between the one for each button. Also made a prefab for the GUI
    so that it can be transferred between scenes easily.

---
+++ Assets/Scripts/Menus/Popup/Popup.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup : MonoBehaviour {
+	public static int WindowType = 0; 	//1 for resources, 2 for troops, 3 for notifications, 0 for nothing
+	GameObject resources, troops, notifications;
+	void Start () {
+		resources = GameObject.Find ("Resources");
+		troops = GameObject.Find ("Troops");
+		notifications = GameObject.Find ("Notifications");
+		Visibility ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void SwitchWindowType (int x){
+		if (WindowType == x) {
+			WindowType = 0;
+		} else {
+			WindowType = x;
+		}
+		Visibility ();
+
+	}
+	public int GetWindowType(){
+		return WindowType;
+	}
+	public void Visibility(){
+		if (WindowType != 0) {
+			gameObject.SetActive(true);
+			//Toggle the gameobjects
+			switch (WindowType){
+				case 1:
+					resources.SetActive(true);
+					troops.SetActive(false);
+					notifications.SetActive(false);
+					break;
+				case 2:
+					resources.SetActive(false);
+					troops.SetActive(true);
+					notifications.SetActive(false);
+					break;
+				case 3:
+					resources.SetActive(false);
+					troops.SetActive(false);
+					notifications.SetActive(true);
+					break;
+				default:
+					break;
+			}
+		}else{
+			gameObject.SetActive(false);
+		}
+	}
+}
